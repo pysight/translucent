a93c2dac934de66eadb567da180f4eded300aec0
Ivan Smirnov <i.s.smirnov@gmail.com>
2013-11-06 01:02:04 +0000

    Added checkbox component

--- examples/example.py
+++ examples/example.py
@@ -12,12 +12,13 @@ ui.set('left')(ui.navlist)
 ui.set('right')(
 
     ui.h4(nav='Panel')['Panel'],
-    ui.panel('Panel title', style='env.style')[
+    ui.panel('Panel title', style='env.style', title='env.title')[
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, modi!'
     ],
     ui.panel[
-        ui.select('style', ['default', 'success', 'info', 'warning', 'danger'],
-            label='Style', init='default')
+        ui.select('style', ['default', 'primary', 'success', 'info', 'warning', 'danger'],
+            label='Heading style', init='primary'),
+        ui.checkbox('title', 'Large title', init=False)
     ],
 )
 
