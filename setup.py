cdb99288f34a49859ef46e01c66fcb1094386b79
Ivan Smirnov <i.s.smirnov@gmail.com>
2014-02-24 21:13:22 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='translucent',
-    version='0.0.3',
+    version='0.0.4',
     description='translucent',
     long_description=open('README.rst', 'rt').read(),
     author='Ivan Smirnov',
