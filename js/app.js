eab6cff26ba3763e9c631c72330e12a2cff4e80d
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-08-16 22:46:15 +0100

    Remove $.ready in app.js, move SyncCallback to syncCallback.js

--- js/app.js
+++ js/app.js
@@ -5,73 +5,55 @@ import Context from './context';
 import Actions from './actions';
 import Store from './store';
 import Components from './components';
+import syncCallback from './syncCallback';
 
 import debug from 'debug';
 debug.enable('translucent');
 const log = debug('translucent');
 
-class SyncCallback {
-    constructor(times, func) {
-        this.called = 0;
-        this.times = times;
-        this.func = func;
-    }
 
-    callback(data) {
-        this.called += 1;
-        this.data = this.data || data;
-        if (this.called == this.times) {
-            this.func(this.data);
+window['_'] = _;
+
+window['req'] = function (mods, cb) {
+    let callback = syncCallback(1 + mods.length, cb);
+    for (let mod of mods) {
+        if (mod == 'bootstrap') {
+            require('bootstrap')(mod => {
+                callback();
+            });
         }
+        if (mod == 'react-bootstrap') {
+            require('react-bootstrap')(mod => {
+                window.ReactBootstrap = mod;
+                callback();
+            });
+        } else if (mod == 'classnames') {
+            require('classnames')(mod => {
+                window.classNames = mod;
+                callback();
+            });
+        }
+    }
+    callback();
+}
+window.Translucent = {
+    render: (func) => {
+        React.render(<Context render={func} />, document.body);
     }
 }
 
-$(() => {
-    window['_'] = _;
+// initialize the store
+Store.getInitialState();
 
-    window['req'] = function (mods, cb) {
-        let sc = new SyncCallback(1 + mods.length, cb);
-        for (let mod of mods) {
-            if (mod == 'bootstrap') {
-                require('bootstrap')(mod => {
-                    sc.callback();
-                });
-            }
-            if (mod == 'react-bootstrap') {
-                require('react-bootstrap')(mod => {
-                    window.ReactBootstrap = mod;
-                    sc.callback();
-                });
-            } else if (mod == 'classnames') {
-                require('classnames')(mod => {
-                    window.classNames = mod;
-                    sc.callback();
-                });
-            }
-        }
-        sc.callback();
-    }
-    window.Translucent = {
-        render: (func) => {
-            React.render(<Context render={func} />, document.body);
-        }
-    }
+const evalJSX = syncCallback(2, data => eval(transform(data, { harmony: true })));
 
-    // initialize the store
-    Store.getInitialState();
+// initialize SockJS connection
+const connection = new Connection(evalJSX);
 
-    const sync = new SyncCallback(2, data => {
-        eval(transform(data, {harmony: true}));
-    });
-
-    // initialize SockJS connection
-    const connection = new Connection(sync.callback.bind(sync));
-
-    // fetch user code from the server and execute when ready
-    $.ajax({
-        url: 'index.js',
-        cache: false,
-        converters: { 'text script': _.identity },
-        success: sync.callback.bind(sync)
-    });
+// fetch user code from the server and execute when ready
+$.ajax({
+    url: 'index.js',
+    cache: false,
+    converters: { 'text script': _.identity },
+    success: evalJSX
 });
---
+++ js/syncCallback.js
@@ -0,0 +1,21 @@
+class SyncCallback {
+    constructor(times, func) {
+        this.called = 0;
+        this.times = times;
+        this.func = func;
+    }
+
+    callback(data) {
+        this.called += 1;
+        this.data = this.data || data;
+        if (this.called == this.times) {
+            this.func(this.data);
+        }
+    }
+}
+
+export default (times, func) => {
+    let sync = new SyncCallback(times, func);
+    return sync.callback.bind(sync);
+}
+
