ab9e793c31922114ae6372cedc288313983c2aee
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-08-23 10:59:20 +0100

    Refactor common code in both entry points and move it to ./app

--- js/app-es5.js
+++ js/app-es5.js
@@ -1,25 +1,3 @@
-import React from 'react';
+import app from './app';
 
-import Connection from './connection';
-import Context from './context';
-import Store from './store';
-import defer from './defer';
-import loader from './loader';
-
-import './components';
-
-window.Translucent = {
-    render: (func) => {
-        React.render(<Context render={func} />, document.body);
-    }
-};
-
-Store.getInitialState();
-
-const evalJSX = defer(2, loader);
-
-const connection = new Connection(evalJSX);
-
-fetch('/index.js')
-    .then(response => response.text())
-    .then(body => evalJSX(body));
+app();
---
+++ js/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Connection from './connection';
+import Context from './context';
+import Store from './store';
+import defer from './defer';
+import loader from './loader';
+
+import './components';
+
+export default function app(transformer) {
+    window.Translucent = {
+        render: (func) => {
+            React.render(<Context render={func} />, document.body);
+        }
+    };
+
+    Store.getInitialState();
+
+    const evalJSX = defer(2, data => loader((transformer || (x => x))(data)));
+
+    const connection = new Connection(evalJSX);
+
+    fetch('/index.js')
+        .then(response => response.text())
+        .then(body => evalJSX(body));
+}
