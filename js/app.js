a6ea2e748c9561b53811f4816ba6c17a404c0a24
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-08-29 21:29:24 +0100

    Remove redundant imports

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import _ from 'underscore';
 
 import defer from './defer';
 import loader from './loader';
 import connect from './connect';
-import Container from './container';
 
 export default function app(transformer) {
     const codeLoader = _.compose(loader, transformer || _.identity);
